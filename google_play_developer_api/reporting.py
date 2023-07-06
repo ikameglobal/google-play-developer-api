693f01d9ab90f98ef24a99c4fc882f45efc0533c
Minh <minhpc@ikameglobal.com>
2023-07-06 17:21:11 +0700

    add daily report

--- google_play_developer_api/reporting.py
+++ google_play_developer_api/reporting.py
@@ -20,7 +20,7 @@ class ReportingService:
         dimensions: list[str] = [],
         metrics: list[str] = [],
         metric_set: str = "",
-        page_size: int = 100000,
+        page_size: int = 50000,
     ) -> list[dict]:
         """
         Get report data from Google Play Developer API
@@ -322,6 +322,180 @@ class ReportingService:
                 "month": end_time.month,
                 "day": end_time.day,
                 "hours": end_time.hour,
+            },
+        }
+
+        return self._get_report_data(
+            app_package_name=app_package_name,
+            timeline_spec=timeline_spec,
+            dimensions=dimensions,
+            metrics=metrics,
+            metric_set=metric_set,
+        )
+
+    def get_anr_rate_report_daily(
+        self,
+        app_package_name: str = "",
+        start_time: str = "YYYY-MM-DD",
+        end_time: str = "YYYY-MM-DD",
+        dimensions: list[str] = [],
+        metrics: list[str] = [],
+    ) -> list[dict]:
+        """
+        Get ANR rate report daily
+
+        Note:
+            Read this doc https://developers.google.com/play/developer/reporting/reference/rest/v1beta1/vitals.anrrate
+
+        Args:
+            app_package_name: App package name
+            start_time: Start time (format YYYY-MM-DD)
+            end_time: End time (format YYYY-MM-DD)
+            dimensions: Dimensions (see docs above)
+            metrics: Metrics (see docs above)
+
+        Returns:
+            List of dicts with report data
+        """
+        dimensions = (
+            [
+                "apiLevel",
+                "deviceBrand",
+                "versionCode",
+                "countryCode",
+                "deviceType",
+                "deviceModel",
+                "deviceRamBucket",
+                "deviceSocMake",
+                "deviceSocModel",
+                "deviceCpuMake",
+                "deviceCpuModel",
+                "deviceGpuMake",
+                "deviceGpuModel",
+                "deviceGpuVersion",
+                "deviceVulkanVersion",
+                "deviceGlEsVersion",
+                "deviceScreenSize",
+                "deviceScreenDpi",
+            ]
+            if not dimensions
+            else dimensions
+        )
+
+        metrics = (
+            [
+                "anrRate",
+                "userPerceivedAnrRate",
+                "distinctUsers",
+            ]
+            if not metrics
+            else metrics
+        )
+        metric_set = "anrRateMetricSet"
+
+        start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d")
+        end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d")
+
+        timeline_spec = {
+            "aggregationPeriod": "DAILY",
+            "startTime": {
+                "year": start_time.year,
+                "month": start_time.month,
+                "day": start_time.day,
+                "timeZone": {"id": "America/Los_Angeles"}
+            },
+            "endTime": {
+                "year": end_time.year,
+                "month": end_time.month,
+                "day": end_time.day,
+                "timeZone": {"id": "America/Los_Angeles"}
+            },
+        }
+
+        return self._get_report_data(
+            app_package_name=app_package_name,
+            timeline_spec=timeline_spec,
+            dimensions=dimensions,
+            metrics=metrics,
+            metric_set=metric_set,
+        )
+
+    def get_crash_rate_report_daily(
+        self,
+        app_package_name: str = "",
+        start_time: str = "YYYY-MM-DD",
+        end_time: str = "YYYY-MM-DD",
+        dimensions: list[str] = [],
+        metrics: list[str] = [],
+    ) -> list[dict]:
+        """
+        Get crash rate report daily
+
+        Note:
+            Read this doc https://developers.google.com/play/developer/reporting/reference/rest/v1beta1/vitals.crashrate/query#request-body
+
+        Args:
+            app_package_name: App package name
+            start_time: Start time (format YYYY-MM-DD)
+            end_time: End time (format YYYY-MM-DD)
+            dimensions: Dimensions (see docs above)
+            metrics: Metrics (see docs above)
+
+        Returns:
+            List of dicts with report data
+        """
+        dimensions = (
+            [
+                "apiLevel",
+                "deviceBrand",
+                "versionCode",
+                "countryCode",
+                "deviceType",
+                "deviceModel",
+                "deviceRamBucket",
+                "deviceSocMake",
+                "deviceSocModel",
+                "deviceCpuMake",
+                "deviceCpuModel",
+                "deviceGpuMake",
+                "deviceGpuModel",
+                "deviceGpuVersion",
+                "deviceVulkanVersion",
+                "deviceGlEsVersion",
+                "deviceScreenSize",
+                "deviceScreenDpi",
+            ]
+            if not dimensions
+            else dimensions
+        )
+
+        metrics = (
+            [
+                "crashRate",
+                "userPerceivedCrashRate",
+                "distinctUsers",
+            ]
+            if not metrics
+            else metrics
+        )
+        metric_set = "crashRateMetricSet"
+
+        start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d")
+        end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d")
+
+        timeline_spec = {
+            "aggregationPeriod": "DAILY",
+            "startTime": {
+                "year": start_time.year,
+                "month": start_time.month,
+                "day": start_time.day,
+                "timeZone": {"id": "America/Los_Angeles"},
+            },
+            "endTime": {
+                "year": end_time.year,
+                "month": end_time.month,
+                "day": end_time.day,
+                "timeZone": {"id": "America/Los_Angeles"},
             },
         }
 
