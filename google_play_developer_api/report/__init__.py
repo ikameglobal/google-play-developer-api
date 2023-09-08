124dad6cf47dd1bef38d044a2dc0ff5e61c5a096
Minh <minhpc@ikameglobal.com>
2023-09-08 11:33:29 +0700

    add mapping

--- google_play_developer_api/report/__init__.py
+++ google_play_developer_api/report/__init__.py
@@ -5,3 +5,13 @@ from .excessive_wake_up_rate import ExcessiveWakeUpRateReport
 from .slow_start_rate import SlowStartRateReport
 from .slow_rendering_rate import SlowRenderingRateReport
 from .stuck_background_wake_lock_rate import StuckBackgroundWakelockRateReport
+
+mapping = {
+    "anr_rate": AnrRateReport,
+    "crash_rate": CrashRateReport,
+    "error_count": ErrorCountReport,
+    "excessive_wake_up_rate": ExcessiveWakeUpRateReport,
+    "slow_rendering_rate": SlowRenderingRateReport,
+    "slow_start_rate": SlowStartRateReport,
+    "stuck_background_wake_lock_rate": StuckBackgroundWakelockRateReport
+}
